4ec803d276f75b22e69a96caac3cd8ab02578396
munishjuneja <mjuneja29@gmail.com>
2016-06-27 13:29:50 +0530

    Add Sub services api

--- api/getsubservicesapi.php
+++ api/getsubservicesapi.php
@@ -1,25 +1,28 @@
 <?php 
-	include '../classes/servicesClass.php';
+	include '../classes/subCategoriesClass.php';
 
-	$getObj = new Service;
+	$getObj = new subCategory;
 	if ($_GET['method']=='getsubservice') {
-		$getObj->service_id = $_GET['id'];	
+		$getObj->id = $_GET['id'];	
 		$result = $getObj->allSubCategories();
 				$storearr = array();
 				$i=0;
+		
 		while($resultintoarray = mysqli_fetch_array($result)){
 			$intermediatearray = array();
 			$intermediatearray['id']=$resultintoarray['id'];
-			$intermediatearray['name']=$resultintoarray['name'];
+			// $intermediatearray['service_id']=$resultintoarray['service_id'];
+			$intermediatearray['sub_category_name']=$resultintoarray['sub_category_name'];
 			$storearr[$i]=$intermediatearray;
 						$i++;
 
 		}
+	
 		$newarry = array(
 						$storearr
 					);
-
-				$finalarray['services']=$storearr;
+				
+				$finalarray['subservices']=$storearr;
 				echo json_encode($finalarray);
 		
 	}
