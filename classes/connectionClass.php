f47ddba290675dd60e5199fad7f23417e5cdc07d
guleriacse14 <silentkiller7860@gmail.com>
2016-06-30 18:54:44 +0530

    added navbar and back btton functionality

--- classes/connectionClass.php
+++ classes/connectionClass.php
@@ -2,7 +2,7 @@
 	 
 	define('DB_SERVER'  , 'localhost');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'tryagain');
+	define('DB_PASSWORD', '13538cse');
 	define('DB_DATABASE', 'local_services');
 	
    	class connection {
