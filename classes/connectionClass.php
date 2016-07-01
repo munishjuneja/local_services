17c4f2fe3b91faf53af0a22cb5f3709f4fafb634
skipper1153 <skipper1153@gmail.com>
2016-07-01 12:40:58 +0530

    connection class change

--- classes/connectionClass.php
+++ classes/connectionClass.php
@@ -2,7 +2,7 @@
 	 
 	define('DB_SERVER'  , 'localhost');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'strongpassword');
+	define('DB_PASSWORD', 'z');
 	define('DB_DATABASE', 'local_services');
 	
    	class connection {
