724a46d31b7f14336b8428ae0b39a6bf2451ba44
munishjuneja <mjuneja29@gmail.com>
2016-07-01 14:16:55 +0530

    get order api

--- classes/adminClass.php
+++ classes/adminClass.php
@@ -15,6 +15,12 @@
 			return $rows;
 		}
 
+		function getServices(){
+			$rows = mysqli_query($this->con,"select login.name as user_name, services.name, sub_categories.sub_category_name, sub_child_categories.sub_child_category_name, user_services.service_address, user_services.status from login, services, sub_categories, sub_child_categories, user_services where login.id = user_services.user_id and sub_categories.service_id = '$this->services.id' and sub_child_categories.sub_category_id ='$this->sub_categories.id' and sub_child_categories.id='$this-> user_services.service_id' and login.id = '$this->user_id'");
+			return $rows;
+
+		}
+
 		function userServices(){
 			$query = mysqli_query($this->con,"SELECT user_id from user_services");
 			return $query;
