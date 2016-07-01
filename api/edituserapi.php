af5fba1e483bac460cdf66084e3b0c8294d076a4
tanzin <ricchog@gmail.com>
2016-07-01 21:05:50 +0530

    Edit users api

---
+++ api/edituserapi.php
@@ -0,0 +1,26 @@
+<?php 
+	include_once '../classes/userInfoClass.php';
+	$obj = new UserInfoClass;
+
+	if ($_GET['method']=='edituserdetails') {
+		$obj->id = $_GET['user_id'];
+		$obj->username = $_GET['username'];
+		$obj->contact = $_GET['contact'];
+		$obj->oldpassword = $_GET['oldpassword'];
+		$obj->newpassword = $_GET['newpassword'];
+		$output = $obj->editUser();
+		if ($output==1) {
+			$arr['result']=1;
+			$arr['message']="Successfully updated";
+
+			echo json_encode($arr);
+		}
+		else{
+			$arr['result']=0;
+			$arr['message']="Incorrect details entered";
+			echo json_encode($arr);
+		}
+
+	}
+
+ ?>
--- classes/userInfoClass.php
+++ classes/userInfoClass.php
@@ -8,6 +8,10 @@
 			var $email;
 			var $password;
 			var $address;
+			var $id;
+			var $username;
+			var $oldpassword;
+			var $newpassword;
 
 			public function getAllUsers(){
 				
@@ -25,6 +29,20 @@
 			public function addUser(){
 				
 				$query = mysqli_query($this->con,"INSERT INTO login(`contact`,`name`,`email`,`password`,`address`) values('$this->contact','$this->name','$this->email','$this->password','$this->address')");
+
+			}
+			public function editUser(){
+				$query = mysqli_query($this->con,"SELECT * FROM login where id='$this->id' and password='$this->oldpassword'");
+				
+				$result = 0;
+				if (mysqli_num_rows($query)>0) {
+					$editquery = mysqli_query($this->con,"UPDATE login set password='$this->newpassword',contact='$this->contact',name ='$this->username' where id='$this->id'");
+					$result=1;
+					return $result;
+				}
+				else{
+					return $result; 
+				}
 
 			}
 	}
